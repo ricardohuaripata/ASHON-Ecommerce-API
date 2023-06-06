9e536f4e42394168c1ca8d6892e5d925c8d37e25
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2023-06-06 23:33:25 +0200

    commit

--- src/config/config.js
+++ src/config/config.js
@@ -3,43 +3,37 @@ import Joi from 'joi';
 
 dotenv.config({ path: '.env' });
 
-const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string()
-      .valid('development')
-      .required(),
-    PORT: Joi.number().default(3000),
-    DATABASE_CONNECTION: Joi.string().required().description('MongoDB URL'),
-    DATABASE_PASSWORD: Joi.string().required().description('MongoDB Password'),
-    JWT_SECRET: Joi.string().required().description('JWT Secret Key'),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
-      .default(30)
-      .description('Minutes After Which Access Tokens Expire'),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
-      .default(30)
-      .description('Days After Which Refresh Tokens Expire'),
-    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('Minutes After Which Reset Password Token Expires'),
-    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('Minutes After Which Verify Email Token Expires'),
-    CLIENT_EMAIL: Joi.string().description('Gmail Client Email'),
-    CLIENT_ID: Joi.string().description('Gmail Client ID'),
-    CLIENT_SECRET: Joi.string().description('Gmail Client Secret'),
-    REDIRECT_URI: Joi.string().description('Gmail Redirect URI'),
-    REFRESH_TOKEN: Joi.string().description('Gmail Refresh Token'),
-    CLOUD_NAME: Joi.string().description('Cloudinary Storage Name'),
-    CLOUD_API_KEY: Joi.string().description('Cloudinary Api Key'),
-    CLOUD_API_SECRET: Joi.string().description('Cloudinary Api Secret'),
-    CLOUD_PROJECT: Joi.string().description('Projct Folder'),
-    STRIPE_SECRET_KEY: Joi.string().description('Stripe Secret Key')
-  })
-  .unknown();
+const envVarsSchema = Joi.object().keys({
+  NODE_ENV: Joi.string().valid('development').required(),
+  PORT: Joi.number().default(3000),
+  DATABASE_CONNECTION: Joi.string().required().description('MongoDB URL'),
+  DATABASE_PASSWORD: Joi.string().required().description('MongoDB Password'),
+  JWT_SECRET: Joi.string().required().description('JWT Secret Key'),
+  JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
+    .default(30)
+    .description('Minutes After Which Access Tokens Expire'),
+  JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
+    .default(30)
+    .description('Days After Which Refresh Tokens Expire'),
+  JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
+    .default(10)
+    .description('Minutes After Which Reset Password Token Expires'),
+  JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
+    .default(10)
+    .description('Minutes After Which Verify Email Token Expires'),
+  CLIENT_EMAIL: Joi.string().description('Gmail Client Email'),
+  CLIENT_ID: Joi.string().description('Gmail Client ID'),
+  CLIENT_SECRET: Joi.string().description('Gmail Client Secret'),
+  REDIRECT_URI: Joi.string().description('Gmail Redirect URI'),
+  REFRESH_TOKEN: Joi.string().description('Gmail Refresh Token'),
+  CLOUD_NAME: Joi.string().description('Cloudinary Storage Name'),
+  CLOUD_API_KEY: Joi.string().description('Cloudinary Api Key'),
+  CLOUD_API_SECRET: Joi.string().description('Cloudinary Api Secret'),
+  CLOUD_PROJECT: Joi.string().description('Projct Folder'),
+  STRIPE_SECRET_KEY: Joi.string().description('Stripe Secret Key'),
+}).unknown();
 
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: 'key' } })
-  .validate(process.env);
+const { error, value: envVars } = envVarsSchema.validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -48,11 +42,11 @@ if (error) {
 const config = {
   env: envVars.NODE_ENV,
   server: {
-    port: envVars.PORT
+    port: envVars.PORT,
   },
   db: {
     url: envVars.DATABASE_CONNECTION,
-    password: envVars.DATABASE_PASSWORD
+    password: envVars.DATABASE_PASSWORD,
   },
   jwt: {
     secret: envVars.JWT_SECRET,
@@ -60,26 +54,26 @@ const config = {
     refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
     resetPasswordExpirationMinutes:
       envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
-    verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES
+    verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
   },
   email: {
     from: envVars.CLIENT_EMAIL,
     client: {
       id: envVars.CLIENT_ID,
-      secret: envVars.CLIENT_SECRET
+      secret: envVars.CLIENT_SECRET,
     },
     RedirectUri: envVars.REDIRECT_URI,
-    RefreshToken: envVars.REFRESH_TOKEN
+    RefreshToken: envVars.REFRESH_TOKEN,
   },
   cloud: {
     name: envVars.CLOUD_NAME,
     api_key: envVars.CLOUD_API_KEY,
     api_secret: envVars.CLOUD_API_SECRET,
-    project: envVars.CLOUD_PROJECT
+    project: envVars.CLOUD_PROJECT,
   },
   stripe: {
-    secret_key: envVars.STRIPE_SECRET_KEY
-  }
+    secret_key: envVars.STRIPE_SECRET_KEY,
+  },
 };
 
 export default config;
